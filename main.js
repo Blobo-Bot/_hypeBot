d11ed5fb159af4e84af29eafabe4170a56d897ce
KiuyOR <kiuy.or@icloud.com>
2020-06-02 23:51:05 +0200

    up

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 require('./modules/functions');
-const {Client} = require('discord.js');
+const {Client} = im('discord.js');
 
 const hype = new Client({
     ws: { intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES", "DIRECT_MESSAGES", "GUILD_PRESENCES"] }
